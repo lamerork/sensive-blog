67eb587529f729902d5be4189c574ae49370e7c4
Anton Ozerov <lamerork@gmail.com>
2023-09-05 20:24:09 +0300

    Show popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -27,9 +27,17 @@ def serialize_tag(tag):
     }
 
 
+def get_likes_count(post):
+    return post.likes.count()
+
+
 def index(request):
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    posts = Post.objects.all()
+    popular_posts = sorted(posts, key=get_likes_count)
+    most_popular_posts = popular_posts[-5:]
+
+    #most_popular_posts = []  # TODO. Как это посчитать?
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
